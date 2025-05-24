0be98738e4c52d7163cb9c005761259061d44bff
Octave Graf <cobra.morilles-0n@icloud.com>
2025-05-24 21:22:06 +0200

    added file

---
+++ ft_printf.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/23 11:13:03 by ocgraf            #+#    #+#             */
+/*   Updated: 2025/05/23 11:49:00 by ocgraf           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	detect_conversion(char **str, int *j)
+{
+	
+}
+
+int	ft_printf(const char *str, ...)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	j = 0;
+	while (str[++i])
+	{
+		if str[i] == '%'
+			detect_conversion(&str[i]);
+	}
+}
