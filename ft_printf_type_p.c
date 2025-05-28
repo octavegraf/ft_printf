25ff90dbc11058a3a9db8b290db8da30ca4f0946
Octave Graf <cobra.morilles-0n@icloud.com>
2025-05-28 15:49:30 +0200

    now using stack instead of heap

--- ft_printf_type_p.c
+++ ft_printf_type_p.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 08:39:11 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/05/25 11:15:06 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/05/28 10:28:58 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,13 @@
 
 void	ft_printf_type_p(int *j, void *ptr)
 {
-	char					*result;
+	char					result[15];
 	char					*hexa;
 	unsigned long long int	mem;
 	int						i;
 
 	mem = (unsigned long long int)ptr;
 	i = 14;
-	result = malloc(sizeof(char) * (14 + 1));
 	hexa = "0123456789abcdef";
 	if (!result)
 		return ;
