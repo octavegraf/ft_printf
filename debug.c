7f53c388dea4f75fb3dae3cc027ccbcaf54bbf6b
Octave Graf <cobra.morilles-0n@icloud.com>
2025-06-01 18:03:36 +0200

    added for debugging

---
+++ debug.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/01 11:43:27 by ocgraf            #+#    #+#             */
+/*   Updated: 2025/06/01 12:02:59 by ocgraf           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	main(void)
+{
+	char	*test = "";
+	int		ft_printf_len = 0;
+	int		printf_len = 0;
+
+	ft_printf_len = ft_printf("%c\n", 'a');
+	printf_len = printf("%c\n", 'a');
+	printf("\nft_printf lenght = %d, printf lenght = %d\n\n———————————————\n\n",
+		ft_printf_len, printf_len);
+	ft_printf_len = ft_printf("%s\n", "Ceci est un test.");
+	printf_len = printf("%s\n", "Ceci est un test.");
+	printf("\nft_printf lenght = %d, printf lenght = %d\n\n———————————————\n\n",
+		ft_printf_len, printf_len);
+	ft_printf_len = ft_printf("%p\n", test);
+	printf_len = printf("%p\n", test);
+	printf("\nft_printf lenght = %d, printf lenght = %d\n\n———————————————\n\n",
+		ft_printf_len, printf_len);
+	ft_printf_len = ft_printf("%d, %i; %d, %i\n", INT_MIN, INT_MIN, INT_MAX,
+			INT_MAX);
+	printf_len = printf("%d, %i; %d, %i\n", INT_MIN, INT_MIN, INT_MAX, INT_MAX);
+	printf("\nft_printf lenght = %d, printf lenght = %d\n\n———————————————\n\n",
+		ft_printf_len, printf_len);
+	ft_printf_len = ft_printf("%x, %X; %x, %X\n", INT_MIN, INT_MIN, INT_MAX,
+			INT_MAX);
+	printf_len = printf("%x, %X; %x, %X\n", INT_MIN, INT_MIN, INT_MAX, INT_MAX);
+	printf("\nft_printf lenght = %d, printf lenght = %d\n\n———————————————\n\n",
+		ft_printf_len, printf_len);
+	ft_printf_len = ft_printf("%%\n");
+	printf_len = printf("%%\n");
+	printf("\nft_printf lenght = %d, printf lenght = %d\n\n———————————————\n\n",
+		ft_printf_len, printf_len);
+	return (0);
+}
