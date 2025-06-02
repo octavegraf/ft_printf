8decec8a81e2b9b135c06a6b9fc6b87897c64c89
Octave Graf <ocgraf@student.42.fr>
2025-06-02 12:43:24 +0200

    modified pointer to correspond to real printf

--- ft_printf_type_p.c
+++ ft_printf_type_p.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 08:39:11 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/06/01 17:51:20 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/06/02 12:21:32 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	ft_printf_type_p(int *j, void *ptr)
 {
-	char					result[12];
+	char					result[9];
 	char					*hexa;
 	unsigned long long int	mem;
 	int						i;
 
 	mem = (unsigned long long int)ptr;
-	i = 11;
+	i = 8;
 	hexa = "0123456789abcdef";
 	while (--i >= 0)
 	{
@@ -28,7 +28,7 @@ void	ft_printf_type_p(int *j, void *ptr)
 		mem /= 16;
 	}
 	result[1] = 'x';
-	result[11] = '\0';
+	result[8] = '\0';
 	ft_putstr(result);
-	*j += 11;
+	*j += 8;
 }
