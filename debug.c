1e2dc07ae8244ef515bcea532f221c1aca448afc
Octave Graf <ocgraf@student.42.fr>
2025-06-02 15:12:02 +0200

    debug finished and now commented

--- debug.c
+++ debug.c
@@ -6,17 +6,22 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 11:43:27 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/06/01 18:20:14 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/06/02 15:08:20 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+/* #include "ft_printf.h"
 
 int	main(void)
 {
 	char	*test = "";
 	int		ft_printf_len = 0;
 	int		printf_len = 0;
+
+	ft_printf_len = ft_printf("%p\n", (void *)ULONG_MAX);
+	printf_len = printf("%p\n", (void *)ULONG_MAX);
+	printf("\nft_printf lenght = %d, printf lenght = %d\n\n———————————————\n\n",
+		ft_printf_len, printf_len);
 
 	ft_printf_len = ft_printf("%c\n", 'a');
 	printf_len = printf("%c\n", 'a');
@@ -35,9 +40,9 @@ int	main(void)
 	printf_len = printf("%d, %i; %d, %i\n", INT_MIN, INT_MIN, INT_MAX, INT_MAX);
 	printf("\nft_printf lenght = %d, printf lenght = %d\n\n———————————————\n\n",
 		ft_printf_len, printf_len);
-	ft_printf_len = ft_printf("%x, %X; %x, %X\n", INT_MIN, INT_MIN, INT_MAX,
-			INT_MAX);
-	printf_len = printf("%x, %X; %x, %X\n", INT_MIN, INT_MIN, INT_MAX, INT_MAX);
+	ft_printf_len = ft_printf("%x, %X; %x, %X\n", -10, -10, -10,
+			-10);
+	printf_len = printf("%x, %X; %x, %X\n", -10, -10, -10, -10);
 	printf("\nft_printf lenght = %d, printf lenght = %d\n\n———————————————\n\n",
 		ft_printf_len, printf_len);
 	ft_printf_len = ft_printf("%%\n");
@@ -46,3 +51,4 @@ int	main(void)
 		ft_printf_len, printf_len);
 	return (0);
 }
+ */
